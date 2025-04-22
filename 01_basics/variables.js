3c92c4034ee6906018bf5d800f6a9303ff5ed116
Nisha Kumari <157873086+night30nk@users.noreply.github.com>
2025-04-22 18:29:41 +0000

    variable

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const number = 1234 // can't change because it's constant
+
+let email = "nisha@gmail.com" // can change because it's variable
+
+var emailID = "night@www.com" // we generally don't use this because of block issue and functional scope
+
+city = "Delhi" // we can also assign the variables as it is without stating it's type (it's generally not used too)
+
+// number = 4567 // this will show error because we are trying to change the contant variable
+
+email = "change@gmail.com" // variables can be changed
+
+let people // we don't assign anything to this variable 
+
+console.log(number) // print the constant number 
+
+console.log(email) // print the changes value of email 
+
+console.table([number, email, emailID, city, people]) // this is used to reduce our writeup and get the things in tabular form
