e548ee03b0b9ac651e5f7f1a29388d991b212183
Nisha Kumari <157873086+night30nk@users.noreply.github.com>
2025-04-25 14:34:55 +0000

    25-04-2025

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,7 +1,7 @@
 const number = 1234 // can't change because it's constant
 let email = "nisha@gmail.com" // can change because it's variable
 var emailID = "night@www.com" // we generally don't use this because of block issue and functional scope
-city = "Delhi" // we can also assign the variables as it is without stating it's type (it's generally not used too)
+city = "Delhi" // we can also assign the variables as it is without stating it's type (it's generally not used too) 
 
 // number = 4567 // this will show error because we are trying to change the contant variables
 
