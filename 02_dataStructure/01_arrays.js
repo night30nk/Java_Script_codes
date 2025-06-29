d66ab8ed902e1ac08f8af6ff1be73758cd7fa3c5
Nisha Kumari <157873086+night30nk@users.noreply.github.com>
2025-06-29 12:15:02 +0000

    29-226-2025

--- 02_dataStructure/01_arrays.js
+++ 02_dataStructure/01_arrays.js
@@ -2,6 +2,7 @@
 
 const myArr = [10, 20, 30, 23, 34]
 // console.log(myArr);
+// push , pop , array size is not fixed it's dynamic in nature, and hence can be changed
 
 myArr.unshift(90); // it adds the values in the starting of the index
 myArr.shift(); // it deleted the starting element of the array
@@ -30,4 +31,43 @@ console.log(arr2)
 console.log("B = " + myArr); // here the remainig part will be there, after splicing it from index 1 to 3
 
 
+// talking about other operations in the array 
+// suppose we are having two diff arrays. and we want their data to get combined. -> don't use push
+
+const a1 = [1,2,3,4,5]
+const a2 = [10,20,30,40]
+// a1.push(a2);
+
+console.log(a1); // [ 1, 2, 3, 4, 5, [ 10, 20, 30, 40 ] ] this output will be print -> it assumes the whole array as a particular element
+
+// so concatenate correctly like [1,2,3,4,5,10,20,30,40] 
+const combined = a1.concat(a2);
+console.log(combined); // this will show the combined array correctly
+
+// but we don't use concat , like, what if we want to combine so many datas together ?
+
+const combined2 = [... a1, ... a2]; // three dots signifies the "spread" operator which is used to spread all the elements present in either of them
+// this spread operator will let me used the values in between too, where concat doesn't
+
+console.log(combined2) // if there would be a3 to be combined it can be written as a same syntax.
+
+// flat() is used to flat something, haha, I mean to say, if there are nested arrays present, then it will return the single array
+// it will seperate the current array with commas, and each element will have seperate index
+
+const flatArr = [1,2,3, [4,5,6], [10,20,[30,40]]];
+
+const newFlat = flatArr.flat(Infinity); // here we can pass the arguments as depth, like, till how much depth, we want to flat
+// by default flat() flats the array till depth 1 only 
+// console.log(newFlat); // so if depth is 1 then output will be -> [ 1, 2, 3, 4, 5, 6, 10, 20, [ 30, 40 ] ]
+
+// to increase the depth of flatiness, we can write "Infinitty" as pass as a argument
+console.log(newFlat)
+
+
+// To check whether, something is Arrays or not. 
+console.log(Array.isArray("nisha kumari")) // prints false, becuase this is string
+
+console.log(Array.from("nisha kumari")) // it will create the array as of our choice, or convert it' data type to our array.
+
+console.log(Array.of(1,2,3,3,34,4,4,5)) // create a array I guess. -> yes, it's creates a whole array.
 
